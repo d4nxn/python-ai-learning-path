1873549da150db17d19b3d9bb2dda7885592f3af
Maxi <maksim.hutsau@gmail.com>
2026-06-28 11:26:39 +0200

    fix: debugged program

--- sprints/01-python-basics/exercises/04_debugging.py
+++ sprints/01-python-basics/exercises/04_debugging.py
@@ -5,21 +5,22 @@
 # 2. Возле исправленного места напиши: тип ошибки, причина, исправление.
 # 3. Не переписывай всё сразу с помощью AI.
 
-name = input("What is your name? "
+name = input("What is your name? ") #не была закрыта скобка
 print(f"Hello, {name}!")
 
-age = input("How old are you? ")
+age = int(input("How old are you? "))#добавили тип input'а
 next_age = age + 1
-print("Next year:", next_age)
+print(f"Next age: {next_age}")
 
 price = float(input("Price: "))
 if price > 0:
-print("Accepted")
+    print("Accepted")#не было нужных пробелов
 else:
     print("Price must be positive")
 
 numbers = [1, 2, 3, 4]
 for number in numbers:
-    if number > 2
-        print(numbers)
+    if number > 2: #не было двоеточия
+        print(number)
+
 
