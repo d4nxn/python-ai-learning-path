4dc8727dff4b75e11e064ea3eb6f478918745cba
Maxi <maksim.hutsau@gmail.com>
2026-07-08 17:45:01 +0200

    learn: practice dictionaries and sets

--- sprints/02-collections/exercises/03_dicts_sets.py
+++ sprints/02-collections/exercises/03_dicts_sets.py
@@ -4,6 +4,13 @@
 # Создай словарь user с ключами:
 # name, age, city.
 # Выведи каждое значение отдельной строкой.
+user = {
+    'name': 'Belarus',
+    'age': 1200,
+    'city': 'Minsk'
+}
+for value in user.values():
+    print(value)
 
 
 # Задание 2.
@@ -15,6 +22,20 @@
 # Используй .get().
 
 
+limits = {
+    'food': 500,
+    'transport': 200,
+    'fun': 150
+}
+choice = input("What's the category? ")
+limit = limits.get(choice)
+if choice in limits:
+    print(limit)
+else:
+    print("No limit for this category.")
+
+
+    
 # Задание 3.
 # Даны транзакции:
 # transactions = [
@@ -24,19 +45,40 @@
 # ]
 # Посчитай общую сумму расходов.
 
+transactions = [
+    {"category": "food", "amount": 25},
+    {"category": "transport", "amount": 10},
+    {"category": "food", "amount": 40},
+ ]
+
+summa = 0
+for transaction in transactions:
+    summa += transaction["amount"]
+print(summa)
 
 # Задание 4.
 # Для transactions из задания 3 посчитай сумму только по категории "food".
-
+food_total = 0
+for transaction in transactions:
+    if transaction["category"] == "food":
+        food_total += transaction["amount"]
+print(food_total)
 
 # Задание 5.
 # Даны категории:
 # categories = ["food", "food", "transport", "fun", "transport"]
 # Создай множество уникальных категорий и выведи его.
+categories = ["food", "food", "transport", "fun", "transport"]
+print(set(categories))
 
 
 # Задание 6.
 # Спроси у пользователя 5 слов.
 # Сохрани только уникальные слова через set.
 # Выведи количество уникальных слов.
-
+words = set()
+for _ in range(5):
+    users = input("What's the word? ")
+    if users not in words:
+        words.add(users)
+print(len(words))
